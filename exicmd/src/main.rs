f4df7b0f2890430460b00a2b79d8592a78028920
Sam Willcocks <sam@wlcx.cc>
2024-09-22 14:04:25 +0100

    wip quickxml

--- exicmd/src/main.rs
+++ exicmd/src/main.rs
@@ -1,12 +1,22 @@
-use exi;
+use exi::{
+    self,
+    decoder::quickxml::{self, QuickXMLIterator},
+};
 use log::{self, LevelFilter};
-use std::{fs::File, io::Read, path::PathBuf};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, BufWriter, Read, Write},
+    path::PathBuf,
+};
 use thiserror::Error;
 
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(version, about)]
+#[command(version)]
+/// An EXI (Efficient eXtensible Interchange) utility.
+///
+/// A command line tool for decoding and (eventually) encoding EXI-encoded XML documents.
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -18,7 +28,16 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Decode { file: PathBuf },
+    /// Decode an EXI-encoded document
+    Decode {
+        /// File to read from. Defaults to stdin.
+        in_file: Option<PathBuf>,
+        /// File to output to. Defaults to stdout.
+        out_file: Option<PathBuf>,
+        /// Pretty print output (indented)
+        #[arg(short, long)]
+        pretty: bool,
+    },
 }
 
 #[derive(Error, Debug)]
@@ -40,11 +59,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     env_logger::builder().filter_level(level).init();
     match cli.command {
-        Commands::Decode { file } => {
+        Commands::Decode {
+            in_file,
+            out_file,
+            pretty,
+        } => {
             let mut buf = Vec::new();
-            File::open(file)?.read_to_end(&mut buf)?;
-            for e in exi::decoder::decode(&buf)?.body {
-                log::info!("{:?}", e);
+            let mut input: Box<dyn BufRead> = match in_file {
+                None => Box::new(BufReader::new(io::stdin())),
+                Some(f) => Box::new(BufReader::new(File::open(f)?)),
+            };
+            input.read_to_end(&mut buf)?;
+            let output: Box<dyn Write> = match out_file {
+                None => Box::new(BufWriter::new(io::stdout())),
+                Some(f) => Box::new(BufWriter::new(File::open(f)?)),
+            };
+            let mut w = if pretty {
+                quick_xml::Writer::new_with_indent(output, b' ', 4)
+            } else {
+                quick_xml::Writer::new(output)
+            };
+            for ev in exi::decoder::decode(&buf)?.body.into_iter().to_quick_xml() {
+                w.write_event(ev)?;
             }
         }
     }
