1359af612893ad7ef653068fed622a814cb4138a
Sam Willcocks <sam@wlcx.cc>
2026-02-28 02:20:39 +0000

    fix doctests

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //!     let mut buf = Vec::new();
 //!     File::open("test/notebook.xml.exi".to_string()).unwrap().read_to_end(&mut buf).unwrap();
-//!     let decoded = exi::decoder::decode(&buf).unwrap();
+//!     let decoded = exi::decoder::decode(&buf, None).unwrap();
 //!     println!("Decoded EXI version {:?} with {} events", decoded.header.version, decoded.body.len())
 /// EXI Decoder
 pub mod decoder;
